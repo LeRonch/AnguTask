f1655ac43739fdec55a813229cf92abf4e503dc7
LeRonch <ronan.perrin@hotmail.fr>
2021-09-28 12:09:46 +0200

    bonjour

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task";
+
+export const TASKS: Task[] = [
+    {
+      id: 1,
+      text: 'RDV Dentiste',
+      day: 'Le 19/06 à 11h',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Entretien d\'embauche',
+      day: 'Le 20/07 à 9h30',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Acheter de la patééééeee',
+      day: 'Le 12/12 à 23h56',
+      reminder: false,
+    },
+  ];
